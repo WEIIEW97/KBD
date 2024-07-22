191d80eeccfe7ae92ad6aae71b0c2f6b27f55ffc
william <williamwei9707@gmail.com>
2024-07-22 16:59:43 +0800

    change pipeline for python iterative calls.

--- main.py
+++ main.py
@@ -30,63 +30,70 @@ if __name__ == "__main__":
     cwd = os.getcwd()
     compensate_dist = 400
     scaling_factor = 10
-    apply_global = False
+    # apply_global = False
 
-    camera_types = [
-        "N09ASH24DH0015",
-        "N09ASH24DH0043",
-        "N09ASH24DH0050",
-        "N09ASH24DH0054",
-        "N09ASH24DH0055",
-        "N09ASH24DH0056",
-        "N09ASH24DH0058",
-        "test",
-    ]
-    table_names = [
-        "depthquality-2024-05-20.xlsx",
-        "depthquality_2024-07-08.xlsx",
-        "depthquality_2024-07-09.xlsx",
-        "depthquality-2024-05-22.xlsx",
-        "depthquality-2024-05-22_55.xlsx",
-        "depthquality-2024-05-22_56.xlsx",
-        "depthquality-2024-05-22.xlsx",
-        "depthquality_2024-07-19.xlsx",
-    ]
-    disjoint_depth_ranges = [
-        [601, 3000],
-        [600, 3000],
-        [600, 3000],
-        [600, 2999],
-        [1008, 2908],
-        [600, 3000],
-        [600, 2999],
-        [600, 3000],
-    ]
-    for type, table_name, range_ in zip(
-        camera_types, table_names, disjoint_depth_ranges
-    ):
-        print(f"processing {type} now with {table_name} ...")
-        if type != "test":
-            continue
-        root_dir = f"{cwd}/data/{type}/image_data"
-        copy_dir = f"{cwd}/data/{type}/image_data_transformed_linear"
-        copy_dir2 = f"{cwd}/data/{type}/image_data_transformed_linear2"
-        save_dir = f"{cwd}/data/{type}"
-        tablepath = f"{cwd}/data/{type}/{table_name}"
-        save_params_path = save_dir + "/segmented_linear_KBD_params.json"
-        # sampling_strategy_criterion(root_dir, tablepath, tablepath.replace("depthquality","sampling-criterion"))
-        eval_res, acceptance_rate = eval(root_dir, tablepath)
-        print(f"acceptance rate is {acceptance_rate}")
-        matrix, focal, baseline = generate_parameters_linear(
-            path=root_dir,
-            table_path=tablepath,
-            save_path=save_dir,
-            disjoint_depth_range=range_,
-            compensate_dist=compensate_dist,
-            scaling_factor=scaling_factor,
-            apply_global=apply_global,
-            plot=True,
-        )
+    # camera_types = [
+    #     "N09ASH24DH0015",
+    #     "N09ASH24DH0043",
+    #     "N09ASH24DH0050",
+    #     "N09ASH24DH0054",
+    #     "N09ASH24DH0055",
+    #     "N09ASH24DH0056",
+    #     "N09ASH24DH0058",
+    #     "test",
+    # ]
+    # table_names = [
+    #     "depthquality-2024-05-20.xlsx",
+    #     "depthquality_2024-07-08.xlsx",
+    #     "depthquality_2024-07-09.xlsx",
+    #     "depthquality-2024-05-22.xlsx",
+    #     "depthquality-2024-05-22_55.xlsx",
+    #     "depthquality-2024-05-22_56.xlsx",
+    #     "depthquality-2024-05-22.xlsx",
+    #     "depthquality_2024-07-19.xlsx",
+    # ]
+    # disjoint_depth_ranges = [
+    #     [601, 3000],
+    #     [600, 3000],
+    #     [600, 3000],
+    #     [600, 2999],
+    #     [1008, 2908],
+    #     [600, 3000],
+    #     [600, 2999],
+    #     [600, 3000],
+    # ]
+    root_dir = "D:\william\data\KBD"
+    camera_types = [f for f in os.listdir(root_dir)]
+    disjoint_depth_ranges = [1100, 3000]
+
+    # for type, table_name, range_ in zip(
+    #     camera_types, table_names, disjoint_depth_ranges
+    # ):
+    #     print(f"processing {type} now with {table_name} ...")
+    #     if type != "test":
+    #         continue
+    for apply_global in [True, False]:
+        for camera_type in camera_types:
+            base_path = os.path.join(root_dir, camera_type)
+            file_path = os.path.join(base_path, "image_data")
+            table_name = [f for f in os.listdir(base_path) if f.endswith('.xlsx') and os.path.isfile(os.path.join(base_path, f))][0]
+            table_path = os.path.join(base_path, table_name)
+            copy_path = os.path.join(base_path, "image_data_l")
+
+            # save_params_path = save_dir + "/segmented_linear_KBD_params.json"
+            # sampling_strategy_criterion(root_dir, tablepath, tablepath.replace("depthquality","sampling-criterion"))
+            eval_res, acceptance_rate = eval(file_path, table_path)
+            print(f"acceptance rate is {acceptance_rate}")
+            matrix, focal, baseline = generate_parameters_linear(
+                path=file_path,
+                table_path=table_path,
+                save_path=base_path,
+                disjoint_depth_range=disjoint_depth_ranges,
+                compensate_dist=compensate_dist,
+                scaling_factor=scaling_factor,
+                apply_global=apply_global,
+                plot=True,
+            )
         # k, delta, b, _, _ = generate_parameters(
         #     root_dir, tablepath, save_dir, plot=True
         # )
