341b04ca3c4af6c2aec2a8e2c029d5e509148660
william <williamwei9707@gmail.com>
2024-08-15 10:32:12 +0800

    [c++] modify the main interface api.

--- main.cpp
+++ main.cpp
@@ -31,42 +31,27 @@ namespace po = boost::program_options;
 
 #define rng_end 3000
 
-int main(int argc, char** argv) {
+enum class ReturnStatus : int {
+  NO_NEED_KBD,
+  KBD_AND_PASSED,
+  KBD_BUT_FAILED,
+  KBD_BUT_ORIGINAL_BETTER,
+  ERROR,
+};
 
-  std::string file_path, csv_path, transformed_file_path;
-  bool apply_global = false;
-
-  //========= Handling Program options =========
-  po::options_description desc("Allowed options");
-  desc.add_options()("file_path,f",
-                     po::value<std::string>(&file_path)->default_value(""),
-                     "root directory for the raw data, e.g. 'image_data/'")(
-      "csv_path,c", po::value<std::string>(&csv_path)->default_value(""),
-      "path to the .csv measured information.")(
-      "output_path,t",
-      po::value<std::string>(&transformed_file_path)->default_value(""),
-      "output directory for the transformed data.")(
-      "apply_global,g", po::bool_switch(&apply_global),
-      "apply optimization globally");
-
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, desc), vm);
-  po::notify(vm);
+ReturnStatus pshyco(const std::string& file_path, const std::string& csv_name,
+                    int cy, int cx, bool apply_global = false) {
+  fs::path fs_file_path(file_path);
+  auto base_path = fs_file_path.parent_path();
+  auto csv_path = base_path / csv_name;
+  auto transformed_file_path = base_path / "transformed_data_l";
 
   if (file_path.empty() || csv_path.empty() || transformed_file_path.empty()) {
     fmt::print("You have to make sure arguments are not empty!\n");
-    return 1;
+    return ReturnStatus::ERROR;
   }
-
-  if (vm.count("help")) {
-    std::cout << desc << std::endl;
-    return 1;
-  }
-
-  auto home_path = fs::path(file_path).parent_path().string();
 
   if (!fs::exists(transformed_file_path)) {
-    // Create the directory since it does not exist
     if (fs::create_directory(transformed_file_path)) {
       fmt::print("Directory created successfully: {}\n", transformed_file_path);
     } else {
@@ -78,20 +63,25 @@ int main(int argc, char** argv) {
   kbd::Config default_configs = kbd::Config();
   kbd::JointSmoothArguments args = kbd::JointSmoothArguments();
 
-  args.apply_global = apply_global;
+  if (cy != 0 && cx != 0)
+    default_configs.ANCHOR_POINT = {cy, cx};
 
   kbd::LinearWorkflow workflow;
   std::array<int, 2> search_range = {600, 1100};
   std::array<double, 2> cd_range = {100, 400};
 
-  workflow.preprocessing(file_path, csv_path, default_configs, args);
+  workflow.preprocessing(file_path, csv_path.string(), default_configs, args);
   bool export_original = false;
   auto global_judge = (apply_global ? "global" : "local");
   auto output_json_name = fmt::format(
       "{}_{}.json", default_configs.BASE_OUTPUT_JSON_FILE_NAME_PREFIX,
       global_judge);
-  const std::string dumped_json_path = home_path + "/" + output_json_name;
+  const std::string dumped_json_path =
+      base_path.string() + "/" + output_json_name;
   Eigen::Matrix<double, 5, 5> rm;
+
+  ReturnStatus status = ReturnStatus::ERROR;
+
   if (!workflow.first_check() || !workflow.pass_or_not()) {
     auto [eval_res, acceptance] = workflow.eval();
     std::cout << "acceptance rate: " << acceptance << std::endl;
@@ -114,17 +104,21 @@ int main(int argc, char** argv) {
     if (apply_kbd) {
       auto [disp_nodes, reversed_matrix] =
           workflow.pivot(matrix, best_range, cd);
-      kbd::save_arrays_to_json_debug(dumped_json_path, disp_nodes,
-                                     reversed_matrix, rng_start, cd);
+      kbd::save_arrays_to_json(dumped_json_path, disp_nodes, reversed_matrix);
       rm = reversed_matrix;
       fmt::print("Working done for the optimization part!\n");
+      if (workflow.final_pass_) {
+        status = ReturnStatus::KBD_AND_PASSED;
+      } else {
+        status = ReturnStatus::KBD_BUT_FAILED;
+      }
     } else {
       auto [disp_nodes, reversed_matrix] = workflow.export_default();
-      kbd::save_arrays_to_json_debug(dumped_json_path, disp_nodes,
-                                     reversed_matrix, rng_start, cd);
+      kbd::save_arrays_to_json(dumped_json_path, disp_nodes, reversed_matrix);
       fmt::print("Working done for the optimization part!\n");
       export_original = true;
       rm = reversed_matrix;
+      status = ReturnStatus::KBD_BUT_ORIGINAL_BETTER;
     }
   } else {
     auto [disp_nodes, reversed_matrix] = workflow.export_default();
@@ -132,17 +126,80 @@ int main(int argc, char** argv) {
     fmt::print("Working done for the optimization part!\n");
     export_original = true;
     rm = reversed_matrix;
+    status = ReturnStatus::NO_NEED_KBD;
   }
 
   fmt::print("Begin copying ... \n");
-  kbd::ops::parallel_copy(file_path, transformed_file_path, default_configs);
+  kbd::ops::parallel_copy(file_path, transformed_file_path.string(),
+                          default_configs);
 
   fmt::print("Begin transformation ... \n");
   if (export_original) {
     kbd::ops::parallel_transform(
-        transformed_file_path, rm, workflow.get_focal_val(),
+        transformed_file_path.string(), rm, workflow.get_focal_val(),
         workflow.get_baseline_val(), default_configs, args);
   }
   fmt::print("All tasks done! ... \n");
+
+  return status;
+}
+
+int main(int argc, char** argv) {
+
+  std::string file_path, csv_name;
+  bool apply_global = false;
+  int cy, cx;
+
+  //========= Handling Program options =========
+  po::options_description desc("Allowed options");
+  desc.add_options()("file_path,f",
+                     po::value<std::string>(&file_path)->default_value(""),
+                     "root directory for the raw data, e.g. 'image_data/'")(
+      "csv_name,c", po::value<std::string>(&csv_name)->default_value(""),
+      "path to the .csv measured information.")(
+      "anchor_y,y", po::value<int>(&cy)->default_value(0), "anchor point y")(
+      "anchor_x,x", po::value<int>(&cx)->default_value(0),
+      "anchor point x")("apply_global,g", po::bool_switch(&apply_global),
+                        "apply optimization globally");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << std::endl;
+    return 1;
+  }
+
+  auto return_status = pshyco(file_path, csv_name, cy, cx);
+
+  if (return_status == ReturnStatus::ERROR) {
+    fs::path fs_file_path(file_path);
+    auto base_path = fs_file_path.parent_path();
+    auto error_path = base_path / "error.log";
+
+    std::ofstream out(error_path);
+    if (!out.is_open()) {
+      std::cerr << "failed to open file." << std::endl;
+      return 1;
+    }
+    out << "Configuration is wrong! Application cannot run!" << std::endl;
+    out.close();
+  } else if (return_status == ReturnStatus::KBD_BUT_FAILED) {
+    fs::path fs_file_path(file_path);
+    auto base_path = fs_file_path.parent_path();
+    auto failed_path = base_path / "failed.log";
+
+    std::ofstream out(failed_path);
+    if (!out.is_open()) {
+      std::cerr << "failed to open file." << std::endl;
+      return 1;
+    }
+    out << "This device cannot pass the metric even after kbd. Please recheck "
+           "the device itself or your operation."
+        << std::endl;
+    out.close();
+  }
+
   return 0;
 }
