fixed bugs for invalid pixels mean value computation.

KBD/eval.py
@@ -67,12 +67,21 @@ def ratio_evaluate(alpha: float, df: pd.DataFrame, min_offset: int = 500):
         return True
 
 
-def first_check(df: pd.DataFrame, max_thr: float=0.07, mean_thr: float=0.05):
-    err_rate = np.abs(df[GT_ERROR_NAME] / df[GT_DIST_NAME])
+def first_check(df: pd.DataFrame, max_thr: float = 0.07, mean_thr: float = 0.05):
+    # err_rate = np.abs(df[GT_ERROR_NAME] / df[GT_DIST_NAME])
+
+    err_rate = np.abs((df[AVG_DIST_NAME] - df[GT_DIST_NAME]) / df[GT_DIST_NAME])
+    # print("error double")
+    # print((df[AVG_DIST_NAME]-df[GT_DIST_NAME]))
+    # print("error gt")
+    # print(df[GT_DIST_NAME])
+    print(f"max error rate is {np.max(err_rate)}, mean error rate is {np.mean(err_rate)}")
+
     if np.max(err_rate) < max_thr and 0 <= np.mean(err_rate) < mean_thr:
         return True
     else:
         return False
+
 
 def evaluate_target(
     focal,

KBD/helpers.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 from .constants import *
 from .utils import load_raw, read_table
 
+
 def crop_center(array: np.ndarray, crop_size: Union[tuple, list]):
     if array.ndim != 2:
         raise ValueError("Input array must be a 2D array")
@@ -19,12 +20,14 @@ def crop_center(array: np.ndarray, crop_size: Union[tuple, list]):
     half_crop_size = crop_size // 2
 
     # Calculate start and end indices
+
     start_y = max(0, center_y - half_crop_size)
     end_y = min(height, center_y + half_crop_size)
     start_x = max(0, center_x - half_crop_size)
     end_x = min(width, center_x + half_crop_size)
 
     # Crop and return the central square
+
     return array[start_y:end_y, start_x:end_x]
 
 
@@ -41,6 +44,7 @@ def copy_files_in_directory(src: str, dst: str) -> None:
 def copy_all_subfolders(src: str, dst: str) -> None:
     folders = retrive_folder_names(src)
     # by need of QA team
+
     cam_name = "camparam.txt"
     for folder in tqdm(folders):
         source_path = os.path.join(src, folder, SUBFIX)
@@ -49,7 +53,6 @@ def copy_all_subfolders(src: str, dst: str) -> None:
         cam_source = os.path.join(src, folder, cam_name)
         cam_dest = os.path.join(dst, folder, cam_name)
         shutil.copy2(cam_source, cam_dest)
-
     print("Copying done ...")
 
 
@@ -97,7 +100,27 @@ def calculate_mean_value(
             if is_median:
                 mu = np.median(valid_raw)
             else:
-                mu = np.mean(valid_raw)
+                mu = np.mean(valid_raw, where=valid_raw != 0)
+            mean_dist_holder.append(mu)
+        final_mu = np.mean(mean_dist_holder)
+        dist_dict[distance] = final_mu
+    return dist_dict
+
+
+def get_anchor_point_data(rootpath: str, folders: list[str]):
+    dist_dict = {}
+    for folder in folders:
+        distance = folder.split("_")[0]
+        rawpath = os.path.join(rootpath, folder, SUBFIX)
+        paths = [
+            f for f in os.listdir(rawpath) if os.path.isfile(os.path.join(rawpath, f))
+        ]
+        mean_dist_holder = []
+        for path in paths:
+            path = os.path.join(rawpath, path)
+            raw = load_raw(path, H, W)
+            valid_raw = raw[ANCHOR_POINT[0], ANCHOR_POINT[1]]
+            mu = valid_raw
             mean_dist_holder.append(mu)
         final_mu = np.mean(mean_dist_holder)
         dist_dict[distance] = final_mu
@@ -109,8 +132,22 @@ def map_table(df: pd.DataFrame, dist_dict: dict) -> tuple[float, float]:
     focal = df[FOCAL_NAME].iloc[0]  # assume focal value is the same
     baseline = df[BASLINE_NAME].iloc[0]  # assume basline value is the same
 
-    focal *=  FOCAL_MULTIPLIER  # very dirty hack
+    focal *= FOCAL_MULTIPLIER  # very dirty hack
     df[AVG_DISP_NAME] = focal * baseline / df[AVG_DIST_NAME]
+    # df[GT_DISP_NAME] = focal * baseline / df[GT_DIST_NAME]
+    # df[GT_DISP_ERROR_NAME] = df[GT_DISP_NAME] - df[AVG_DISP_NAME]
+
+    return focal, baseline
+
+def map_table_debug(df: pd.DataFrame, dist_dict1: dict, dist_dict2: dict) -> tuple[float, float]:
+    df[AVG_DIST_NAME] = df[GT_DIST_NAME].astype(str).map(dist_dict1)
+    focal = df[FOCAL_NAME].iloc[0]  # assume focal value is the same
+    baseline = df[BASLINE_NAME].iloc[0]  # assume basline value is the same
+
+    focal *= FOCAL_MULTIPLIER  # very dirty hack
+    df[AVG_DISP_NAME] = focal * baseline / df[AVG_DIST_NAME]
+
+    df["ANCHOR_POINT_DATA"] = df[GT_DIST_NAME].astype(str).map(dist_dict2)
     # df[GT_DISP_NAME] = focal * baseline / df[GT_DIST_NAME]
     # df[GT_DISP_ERROR_NAME] = df[GT_DISP_NAME] - df[AVG_DISP_NAME]
     return focal, baseline
@@ -119,6 +156,8 @@ def map_table(df: pd.DataFrame, dist_dict: dict) -> tuple[float, float]:
 def preprocessing(path, table_path, paid_dict=MAPPED_PAIR_DICT):
     all_distances = retrive_folder_names(path)
     mean_dists = calculate_mean_value(path, all_distances)
+    # s_dists = get_anchor_point_data(path, all_distances)
     df = read_table(table_path, pair_dict=paid_dict)
     focal, baseline = map_table(df, mean_dists)
+    # focal, baseline = map_table_debug(df, mean_dists, s_dists)
     return df, focal, baseline